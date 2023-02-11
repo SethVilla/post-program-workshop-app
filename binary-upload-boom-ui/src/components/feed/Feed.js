51ebf76617d978213b17752782d8035623aa16d0
yusha7 <sufyananderson@gmail.com>
2023-02-10 19:33:49 -0500

    feed update in components

--- binary-upload-boom-ui/src/components/feed/Feed.js
+++ binary-upload-boom-ui/src/components/feed/Feed.js
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useState, useReducer } from 'react';
 import {getRickAndMortyCharacters} from "../../services/services";
 import {useQuery} from "@tanstack/react-query";
 
 export const Feed = () => {
+
+  const feedReducer = (state, action) => {
+    switch(action.type) {
+      case "showList" : {return {...state, showList: !state.showList}}
+    }
+  }
+
+  const {showChar, setShowChar} = useState(false);
+  const [state, dispatch ] = useReducer(feedReducer, {showList: false} )
+
   const { status, data, error } = useQuery({
     queryKey: ['characters'],
     queryFn: getRickAndMortyCharacters,
@@ -19,7 +29,10 @@ export const Feed = () => {
   return <>
 
     <h2>Feed</h2>
-    {data?.data?.results?.map(({id, name}) => (
+    {/* <button onClick= { () => setShowChar}>"Hit me!"</button> */}
+    
+    <button onClick= { () => dispatch({type: "showList"})}>"Hit me!"</button>
+    {state?.showList && data?.data?.results?.map(({id, name}) => (
         <li key={id}>{name}</li>
     ))}
   </>;
