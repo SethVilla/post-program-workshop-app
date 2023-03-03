e793121f4b66c746bff0dd251c5a889473597641
Seth Villa <sethvilla13@gmail.com>
2023-03-03 14:33:58 -0800

    added steps for basic auth, db connection, and env config

--- binary-upload-boom-api/sql/schema.sql
+++ binary-upload-boom-api/sql/schema.sql
@@ -1,5 +1,16 @@
+-- Connect to PSQL
+-- psql -U <username>
+
+-- Create DB
+-- DATABASE <dbname>;
+
+-- Connect to DB
+-- \c <dbname>
+
+-- Add user roles enum
 CREATE TYPE valid_roles AS ENUM ('user', 'admin', 'super_admin');
 
+-- Create Table users
 CREATE TABLE users (
     uid SERIAL PRIMARY KEY,
     first_name VARCHAR(255) NOT NULL,
@@ -9,6 +20,7 @@ CREATE TABLE users (
     role valid_roles DEFAULT 'user' NOT NULL
 );
 
+-- Create Table posts
 CREATE TABLE posts (
     pid SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
@@ -19,8 +31,22 @@ CREATE TABLE posts (
     date_created TIMESTAMP
 );
 
+-- Create Table comments
 CREATE TABLE comments (
   cid SERIAL PRIMARY KEY,
   comment VARCHAR(255) NOT NULL,
   user_id INT references users(uid)
 );
+
+-- DROP TABLE <TABLE NAME> IF YOU WANT TO START OVER
+
+-- Create a .env file in the binary upload boom api folder
+
+-- Paste the following below with your info
+-- DB_USER="<your computer username>"
+-- HOST="localhost"
+-- DATABASE_NAME="<db name>"
+-- PASSWORD="<username password>"
+-- CLOUD_NAME="<cloudinary sdk name>"
+-- CLOUD_API_KEY="<cloudinary api key>"
+-- CLOUD_API_SECRET="<cloudinary secret>"
